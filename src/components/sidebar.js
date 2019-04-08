01ecb990c91e357682931ac9c740c1e98a621cd3
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-04-08 22:19:37 +0500

    Made sidebar responsive

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import './sidebar.css'
 
 const Sidebar = ({ siteTitle }) => {
   return (
     <div className="column">
       <div className="logo">
-        <img src="forDisplay.jpg" alt="" />
-        <h1>{siteTitle}</h1>
+        <img src="dp.jpg" alt="" />
+        <Link to="/">
+          <h2 id="title">{siteTitle}</h2>
+        </Link>
       </div>
       <hr className="hr" />
-      <div className="row">About</div>
-      <div className="row">Blog</div>
-      <div className="row">Portfolio</div>
+      <Link to="/blog">Blog</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <div className="social">
+        <a href="https://www.linkedin.com/in/shoaib-khalid-408791113/">
+          <i className="fab fa-linkedin-in" />
+        </a>
+        <a href="https://www.facebook.com/shoaibkhalid1996">
+          <i className="fab fa-facebook-f" />
+        </a>
+        <a href="https://github.com/shoaibeams">
+          <i className="fab fa-github" />
+        </a>
+        <a href="https://twitter.com/shoaibkhalid_">
+          <i className="fab fa-twitter" />
+        </a>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,18 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <>
     <Layout>
-      <h1>Hello Everyone! :)</h1>
-      <p>Welcome to my new website!</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda explicabo
-        a nam libero ad enim molestias tempora error natus. Eaque cumque animi
-        laudantium vero quidem corrupti libero possimus repellat dolore!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident molestiae
-        voluptatem laudantium sit vero perspiciatis? Magni placeat culpa excepturi
-        quam aspernatur incidunt nisi unde vel dolores doloremque? Quas, incidunt
-        quae.
-      </p>
+      <h2>A little About Me</h2>
+      <p>I am a Full-Stack Developer based in Islamabad, Pakistan.</p>
+      <div>
+        <p>
+          I have an experience of one year working with modern web tools and
+          libraries like React, Redux, GraphQL, MongoDB, NodeJS, and GatsbyJS.
+        </p>
+        <p>
+          I like to break things and then put them back together to learn how they
+          work.
+        </p>
+      </div>
     </Layout>
   </>
 )
